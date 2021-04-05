f2e4338576395d52500d44b73ac7b959ce82b266
NoraElshahat <nelshohat@gmail.com>
2021-04-05 15:39:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8"></div>
+          <div class="col-lg-4"></div>
+        </div>
+      </div>
     </div>
   );
 }
